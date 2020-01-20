63fb7deaabf05103b25f63227637edd8a4d9690b
Robert Mitroi <robert@pop-os.localdomain>
2020-01-20 15:26:10 +0100

    started work for tieles

--- src/components/UI/Tile.vue
+++ src/components/UI/Tile.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
-    <img src="../../assets/pepper-hot-solid.svg" alt="" />
-    <slot />
+  <div class="w-full flex">
+    <div v-for="(tile, index) in tiles" :key="index" class="w-1/2 p-8">
+      <img :src="tile.icon" class="w-4 h-4" />
+      <p>{{ tile.name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    iconPath: {
-      type: String,
-      default: "pepper-hot-solid.svg"
-    }
+  data() {
+    const pepper = require("../../assets/pepper-hot-solid.svg");
+    return {
+      tiles: [
+        { name: "Lists", icon: pepper },
+        { name: "Recipes", icon: pepper },
+        { name: "Products", icon: pepper }
+      ]
+    };
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-   <Title title="Dashboard" infoText="On this page you can access the quick navigation for Products, Lists and Recipes."></Title>
+    <Title
+      title="Dashboard"
+      infoText="On this page you can access the quick navigation for Products, Lists and Recipes."
+    ></Title>
 
-   
-   <Title title="Low On Stock" infoText="Here you can check the products that are on low quantities and update them."></Title>
-    <Navigation></Navigation>
-    <div class="w-full">
-    </div>
+    <Title
+      title="Low On Stock"
+      infoText="Here you can check the products that are on low quantities and update them."
+    ></Title>
+    <Tile></Tile>
     <Navigation></Navigation>
     <div class="w-full"></div>
   </div>
@@ -14,15 +17,16 @@
 
 <script>
 import Navigation from "@/components/Navigation.vue";
-import Title from "@/components/UI/Title.vue"
+import Title from "@/components/UI/Title.vue";
+import Tile from "@/components/UI/Tile.vue";
 export default {
   name: "home",
   components: {
     Navigation,
-    Title
+    Title,
+    Tile
   }
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
