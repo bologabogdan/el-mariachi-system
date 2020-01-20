041d7805aa476ea99343ce3ba298447a228c2242
Robert Mitroi <robert@pop-os.localdomain>
2020-01-20 13:09:11 +0100

    added dynamic button

---
+++ src/components/UI/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="button sm:w-1/2"
+    :class="buttonStyle"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    buttonStyle: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style>
+.button:focus {
+  outline: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Navigation></Navigation>
+    <Button type="button">TEXT</Button>
   </div>
 </template>
 
 <script>
 import Navigation from "@/components/Navigation.vue";
+import Button from "@/components/UI/Button.vue";
 export default {
   name: "home",
   components: {
     Navigation,
+    Button
   }
 };
 </script>
