37e105f62b812ee7dd036dadca31daee8e7db6ff
Robert Mitroi <robert@pop-os.localdomain>
2020-01-21 20:39:25 +0100

    list and recipe overview set up

--- src/components/ListOverview.vue
+++ src/components/ListOverview.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    {{ list.name }}
+    <Title
+      :title="list.name"
+      :infoText="'Here are the instructions for making ' + list.name"
+    ></Title>
+    <div class="flex px-8 text-sm w-full justify-end">
+      <Button buttonStyle="success" class="mr-1 w-1/5">Edit</Button>
+      <Button buttonStyle="success" class="mr-1 w-1/5">Delete</Button>
+    </div>
+    <div v-for="(item, key) in list.items" :key="key">
+      <div class="w-full px-8 pt-4 py-1 flex">
+        <input type="checkbox" class="w-6 h-6" />
+        <p class="pl-4">{{item}}</p>
+      </div>
+    </div>
+    <Navigation></Navigation>
   </div>
 </template>
 
 <script>
+import Title from "@/components/UI/Title.vue";
+import Button from "@/components/UI/Button.vue";
+import Navigation from "@/components/Navigation.vue";
 export default {
-  name: "RecipeOverview",
+  name: "ListOverview",
+  components: {
+    Title,
+    Button,
+    Navigation
+  },
   props: {
     list: {
       type: Object
--- src/components/RecipeOverview.vue
+++ src/components/RecipeOverview.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    {{recipe.name}}
+    <Title
+      :title="recipe.name"
+      :infoText="'Here are the instructions for making ' + recipe.name"
+    ></Title>
+     <div class="flex px-8 text-sm w-full justify-end">
+      <Button buttonStyle="success" class="mr-1 w-1/5">Edit</Button>
+      <Button buttonStyle="success" class="mr-1 w-1/5">Delete</Button>
+    </div>
+    <div class="px-8 pt-4 py-1 ">
+        <h1 class="">{{recipe.description}}</h1>
+    </div>
+    <Navigation></Navigation>
   </div>
 </template>
 
 <script>
+import Title from "@/components/UI/Title.vue";
+import Button from "@/components/UI/Button.vue";
+import Navigation from "@/components/Navigation.vue";
+
 export default {
   name: "RecipeOverview",
+  components: {
+    Title,
+    Button,
+    Navigation
+  },
   props: {
     recipe: {
       type: Object
